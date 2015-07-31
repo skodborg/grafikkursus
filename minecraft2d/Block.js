2c00b62324f9455a9b1f341a5a3716f2db0fa24c
skodborg <skod92@gmail.com>
2015-07-31 11:47:52 +0200

    renamed Box.js to Block.js

---
+++ minecraft2d/Block.js
@@ -0,0 +1,24 @@
+// Block class
+var Block = (function () {
+  
+  function Block(x, y, mat) {
+    this.x = x;
+    this.y = y;
+    this.material = mat;
+    this.color = [mat, mat, mat, mat, mat, mat];
+  }
+
+  //Return the corners of this block, in render list order
+  Block.prototype.getCorners = function() {
+    var ll = gridCoordToBlock(this.x, this.y);
+    var tl = vec2(ll[0], ll[1] + BLOCK_WIDTH);
+    var tr = vec2(ll[0] + BLOCK_WIDTH, ll[1] + BLOCK_WIDTH);
+    var lr = vec2(ll[0] + BLOCK_WIDTH, ll[1]);
+    var result = [ll, tl, tr, lr];
+    return result;
+  }
+
+
+  return Block;
+})();
+
