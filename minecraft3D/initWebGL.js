8ea19122a82dec432d9bb21b91081bee01d8cd79
skodborg <skod92@gmail.com>
2015-08-05 10:56:31 +0200

    added axis lines

---
+++ minecraft3D/initWebGL.js
@@ -0,0 +1,39 @@
+var debugLevel = 1;
+var gl;
+var program;
+window.onload = function() {
+  var canvas = document.getElementById( "gl-canvas" );
+  gl = WebGLUtils.setupWebGL( canvas );
+  if ( !gl ) { alert( "WebGL isn't available" ); }
+
+  // Debug
+  function logGLCall(functionName, args) {   
+   console.log("gl." + functionName + "(" + 
+      WebGLDebugUtils.glFunctionArgsToString(functionName, args) + ")");   
+  } 
+
+  function throwOnGLError(err, funcName, args) {
+    throw WebGLDebugUtils.glEnumToString(err) + " was caused by call to: " + funcName;
+  };
+
+  switch(debugLevel){
+    case 2: // Update gl context to show every WebGL function call
+      gl = WebGLDebugUtils.makeDebugContext(gl, undefined, logGLCall);
+      break;
+    case 1: 
+      gl = WebGLDebugUtils.makeDebugContext(gl, throwOnGLError);
+      break;
+
+  }
+
+    //  Configure WebGL
+  gl.viewport( 0, 0, canvas.width, canvas.height );
+  gl.clearColor( 0.9, 0.9, 0.9, 1.0 );
+
+  
+  //  Load shaders and initialize attribute buffers
+  
+  program = initShaders( gl, "vertex-shader", "fragment-shader" );
+  gl.useProgram( program );
+  init();
+};
--- minecraft3D/minecraft3d.js
+++ minecraft3D/minecraft3d.js
@@ -1,29 +1,38 @@
 var gl;
-var vPositions = [];
-var fColorLoc;
 
-var initPoints = [vec4(0, 0, 0, 1),
-                  vec4(1, 0, 0, 1),
-                  vec4(0.5, 1, 0, 1)];
+var vPositionLoc;
+var vColorLoc;
+
+var axisVertices = [];
+var axisColors = [];
+
+var vBuffer;
+var cBuffer;
 
 function init() {
 
-    vPositions.push(initPoints[0]);
-    vPositions.push(initPoints[1]);
-    vPositions.push(initPoints[2]);
+    // initializes points for painting the axis indicator lines
+    initAxisLines();
 
-    var vBuffer = gl.createBuffer();
+    vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vPositions), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
 
-    var vPositionLoc = gl.getAttribLocation( program, "vPosition" );
+    vPositionLoc = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPositionLoc, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPositionLoc );
 
-    fColorLoc = gl.getUniformLocation( program, "fColor" );
-    gl.uniform4fv(fColorLoc, vec4(1,1,1,1));
+
+
+    cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+
+    // Associate out shader variables with our data buffer
+
+    vColorLoc = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColorLoc, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColorLoc );
 
     render();
 }
@@ -31,6 +40,33 @@ function init() {
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
 
-    gl.drawArrays(gl.TRIANGLES, 0, vPositions.length);
+    // draw XYZ-indicators
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(axisVertices), gl.STATIC_DRAW );
+    gl.vertexAttribPointer( vPositionLoc, 3, gl.FLOAT, false, 0, 0 );
 
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(axisColors), gl.STATIC_DRAW );
+    gl.vertexAttribPointer( vColorLoc, 4, gl.FLOAT, false, 0, 0 );
+
+    gl.drawArrays( gl.LINES, 0, axisVertices.length);
+
+}
+
+function initAxisLines() {
+    axisVertices = [
+        vec3(0.0, 0.0, 0.0),
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 0.0, 0.0),
+        vec3(0.0, 0.0, 1.0)];
+
+    axisColors = [
+        vec4( 1.0, 0.0, 0.0, 1.0 ),
+        vec4( 1.0, 0.0, 0.0, 1.0 ),
+        vec4( 0.0, 1.0, 0.0, 1.0 ),
+        vec4( 0.0, 1.0, 0.0, 1.0 ),
+        vec4( 0.0, 0.0, 1.0, 1.0 ),
+        vec4( 0.0, 0.0, 1.0, 1.0 )];
 }
